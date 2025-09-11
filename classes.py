ab575a028ccfd30d36c4944ffdbffb4ddce76d34
ZeinDanovi <zeindanovi@outlook.com>
2025-09-11 09:26:53 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,21 @@
+# class animals:
+
+#     def __init__(self,legs,paws):
+#         self.paws = paws 
+#         self.legs = legs 
+
+#     def run(self):
+#         print(f"My {self.legs} legs are running")
+
+#     def scratch(self):
+#         print(f"My {self.paws} paws are scratching you")
+
+
+class cars: 
+    
+    def __init__(self,model,numWheels):
+        self.model = model 
+        self.numwheels = numWheels
+
+    def drive(self):
+        print({my})
