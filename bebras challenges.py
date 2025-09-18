aa7e5c1b62349e01c41825150e5d0094609ada3f
ZeinDanovi <zeindanovi@outlook.com>
2025-09-18 22:20:07 +0100

    Add files via upload

---
+++ bebras challenges.py
@@ -0,0 +1,12 @@
+# tram = input()        #TRAM CHALLENGE 
+# bobs = tram.split(",")
+# total = 0
+# for i in range(0,len(bobs)):
+#     stops = bobs[i].split(" ")
+#     difference = int(stops[0]) - int(stops[1])
+#     total = total + difference
+# print(total)
+
+mangos = input()
+test = mangos.splitlines()
+print(test)
